dc7f62c58a7cbc9f3977e133e339682e1af23dc5
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2024-12-26 11:25:40 +0100

    ex03

---
+++ Python-1-Array/ex02/tester.py
@@ -0,0 +1,5 @@
+from load_image import ft_load
+try:
+    print(ft_load("landscape.jpg"))
+except Exception as e:
+    print(e)
---
+++ Python-1-Array/ex03/zoom.py
@@ -0,0 +1,28 @@
+from load_image import ft_load
+import matplotlib.pyplot as plt
+import numpy as np
+
+def rgb2gray(rgb: np.ndarray) -> np.ndarray:
+    """
+    Converts an RGB image to grayscale using the luminosity method.
+    """
+    return (0.2989 * rgb[:, :, 0] + 0.5870 * rgb[:, :, 1] + 0.1140 * rgb[:, :, 2]).astype(int)
+
+
+def main():
+    try:
+        img = ft_load("animal.jpg")
+        print(img)
+        mid_y = img.shape[0] // 2
+        mid_x = img.shape[1] // 2
+        zoomed = img[mid_y-200:mid_y+200,mid_x-200:mid_x+200,:]
+        gray = rgb2gray(zoomed).transpose()
+        print(f"The shape of the zoomed, gray image: {gray.shape}")
+        print(gray)
+        plt.imshow(gray, cmap='gray')
+        plt.show()
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
