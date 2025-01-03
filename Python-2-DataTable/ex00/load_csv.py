a803ea0d69e60e4f4af7979b2dcb643a6d191a4c
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2025-01-04 00:00:53 +0100

    until ex01

---
+++ Python-2-DataTable/ex00/load_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+def load(path: str)->pd.DataFrame:
+    """reads the csv file into a DaaFrame"""
+    if not os.path.exists(path):
+        print(f"Path: {path} does not exist.")
+        return
+    if not os.access(path, os.R_OK):
+        print(f"No permissions to read path: {path}.")
+        return
+    if os.path.isdir(path):
+        print(f"Path: {path} is a directory.")
+        return
+    else:
+        return pd.read_csv(path)
+   
---
+++ Python-2-DataTable/ex01/aff_life.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from load_csv import load
+import numpy as np
+
+def main():
+    """Plots the life expectnacy of a country"""
+    df = load('life_expectancy_years.csv')
+    x_val = df.columns.tolist()
+    ls = np.arange(int(x_val[0]), int(x_val[-1]) + 1)
+    plt.plot(ls, df.loc['Germany'])
+    plt.xlabel("Year")
+    plt.ylabel("Life expectancy")
+    plt.title("Life expectancy over Years: Germany")
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
