13ec9908335354cbd202bd6088071474ab0ca231
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2025-01-05 10:41:47 +0100

    flake8

--- Python-2-DataTable/ex03/projection_life.py
+++ Python-2-DataTable/ex03/projection_life.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    """plots the life expectancy projected on income for 1900
+    and the development from 1800 to 2000"""
     df_life_expectancy = load('life_expectancy_years.csv')
-    df_income = load('income_per_person_gdppercapita_ppp_inflation_adjusted.csv')
-    
+    df_income = load(
+        'income_per_person_gdppercapita_ppp_inflation_adjusted.csv')
+
     life_exp_1900 = df_life_expectancy.loc[:, '1900']
     income_1900 = df_income.loc[:, '1900']
 
@@ -16,7 +19,7 @@ def main():
     plt.title('1900')
     plt.xscale('log')
 
-    fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(20,10))
+    fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(20, 10))
     axes = axes.flatten()
 
     for idx, year in enumerate(range(1800, 2000, 20)):
@@ -28,7 +31,9 @@ def main():
         ax.set_ylabel("Life expectancy")
         ax.set_title(f'Year {year}')
         ax.set_xscale('log')
-    
+
     plt.show()
+
+
 if __name__ == "__main__":
     main()
