c2622684893cbc852873aa9b29d340b76fb0a5d3
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2025-01-05 10:23:00 +0100

    flake8

--- Python-1-Array/ex01/array2D.py
+++ Python-1-Array/ex01/array2D.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+
 def slice_me(family: list, start: int, end: int):
-    if not isinstance(family, list) or not isinstance(start, int) or not isinstance(end, int):
-        raise ValueError(f"Wrong argument types: <list, int, int>. You passed {type(family)}, {type(start)}, {type(end)}")
+    if (not isinstance(family, list) or not
+            isinstance(start, int) or not isinstance(end, int)):
+        raise ValueError(f"""Wrong argument types: <list, int, int>.
+            You passed {type(family)}, {type(start)}, {type(end)}""")
     np_list = np.array(family)
     print(f"My shape is: {np_list.shape}")
     sliced = np_list[start:end]
